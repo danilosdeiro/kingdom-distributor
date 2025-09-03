7801dfb5b5db462f837aa74196b0ce1279d61532
Danilo Souza Deiro <dansdeiro@gmail.com>
2025-09-02 21:38:56 -0300

    ajustes front

--- client-react/src/services/socket.ts
+++ client-react/src/services/socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
+const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 export const socket = io(URL);
