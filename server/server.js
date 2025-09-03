492efe2e9c5e46089eac8208fc8bb0e5ee3dc11a
Danilo Souza Deiro <dansdeiro@gmail.com>
2025-09-03 00:41:33 -0300

    ajuste titulo

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://meukingdom.vercel.app/"],
+    origin: ["http://localhost:5173", "https://meukingdom.vercel.app"],
     methods: ["GET", "POST"]
   }
 });
