7c6b0f987dd94a39018ea9de4e9ed7b735cc2960
Danilo Souza Deiro <dansdeiro@gmail.com>
2025-09-02 23:50:03 -0300

    mudando o CORS

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://kingdom-distributor.vercel.app"],
+    origin: ["http://localhost:5173", "https://meukingdom.vercel.app"],
     methods: ["GET", "POST"]
   }
 });
